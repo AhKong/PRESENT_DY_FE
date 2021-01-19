e285cecfc96f0eabc931a257e53ea7e98274a6b2
ahkong <ahreum0412@naver.com>
2021-01-20 00:40:19 +0900

    메인 페이지 반응형 적용

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import myImg from './myimg.png'
 import  './css/main.css'
 
 function App() {
+
+
     return (
         <div style={{backgroundColor:"#5B9BD5",width:"100%",height:""}}>
             <div style={{backgroundColor:"#5B9BD5",width:"100%",height:"70px",margin:"auto"}}>
             </div>
-            <div style={{backgroundColor:"#FFC001",width:"70%",height:"300px",margin:"auto",marginTop:"50px"}}>
-
-                <h1>도연아 사랑해^^</h1>
+            <div className="title">
+                <p>벌써 졸업이라니!!!! <br/>우리 마음 알지 졸업 축하해^^👩🏻‍🎓🎉
+                    </p>
             </div>
 
             <div className="menu-warp">
-                <div className="menu">메뉴1</div>
-                <div className="menu">메뉴2</div>
-                <div className="menu">메뉴3</div>
+                <div className="menu first" >
+
+                </div>
+                <div className="menu second">메뉴2</div>
+                <div className="menu third">메뉴3</div>
 
             </div>
 
