dc7a6724248b16fdab053b0784a5b83e1668238c
ahkong <ahreum0412@naver.com>
2021-01-21 23:08:17 +0900

    set router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import myImg from './myimg.png'
-import  './css/main.css'
-
+import { Route } from 'react-router-dom';
+import MainPage from "./page/mainPage";
 function App() {
 
 
     return (
-        <div style={{backgroundColor:"#5B9BD5",width:"100%",height:""}}>
-            <div style={{backgroundColor:"#5B9BD5",width:"100%",height:"70px",margin:"auto"}}>
-            </div>
-            <div className="title">
-                <p>벌써 졸업이라니!!!! <br/>우리 마음 알지 졸업 축하해^^👩🏻‍🎓🎉
-                    </p>
-            </div>
-
-            <div className="menu-warp">
-                <div className="menu first" >
-
-                </div>
-                <div className="menu second">메뉴2</div>
-                <div className="menu third">메뉴3</div>
-
-            </div>
-
-            <div className="footer">
-                dev ahkong
-            </div>
+        <div>
+            <Route path="/" component={MainPage} exact/>
 
         </div>
+
     );
 }
 
---
+++ src/page/mainPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import '../css/main.css';
+
+const MainPage = () => {
+    return (
+        <div style={{backgroundColor:"#5B9BD5",width:"100%",height:""}}>
+            <div style={{backgroundColor:"#5B9BD5",width:"100%",height:"70px",margin:"auto"}}>
+            </div>
+            <div className="title">
+                <p>벌써 졸업이라니!!!! <br/>우리 마음 알지 졸업 축하해^^👩🏻‍🎓🎉
+                </p>
+            </div>
+
+            <div className="menu-warp">
+                <div className="menu first" >
+
+                </div>
+                <div className="menu second">메뉴2</div>
+                <div className="menu third">메뉴3</div>
+
+            </div>
+
+            <div className="footer">
+                dev ahkong
+            </div>
+
+        </div>
+    )
+}
+
+export default MainPage;
