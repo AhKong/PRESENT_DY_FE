d62505daf3116c8b335db91ac3bdb7e86e994c48
ahkong <ahreum0412@naver.com>
2021-01-30 00:14:16 +0900

    memory page layout

--- src/page/memoryPage.js
+++ src/page/memoryPage.js
@@ -1,7 +1,66 @@
 import React from "react";
-
+import '../css/memory.css';
+import { Carousel ,List, Avatar, Space} from 'antd';
 const MemoryPage = () =>{
-    return <h1>도연이가 비밀번호 풀면 올 수 있는 페이지</h1>
+    const listData = [];
+    for (let i = 0; i < 3; i++) {
+        listData.push({
+            href: 'https://ant.design',
+            title: `ant design part ${i}`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+
+            content:
+                '잠와',
+        });
+    }
+
+
+
+    return (
+        <div className="memory-warp">
+            <div className="memoryTitle">
+                <h1>남서울대 최고의 아웃풋 === 이도연</h1>
+            </div>
+            <div>
+                <Carousel autoplay>
+                    <div>
+                        <img className="carouselImg" src="https://cdnweb01.wikitree.co.kr/webdata/editor/201710/15/img_20171015122006_4a43f846.jpg"/>
+                    </div>
+                    <div>
+                        <img className="carouselImg" src="https://cdnweb01.wikitree.co.kr/webdata/editor/201710/15/img_20171015122006_4a43f846.jpg"/>
+                    </div>
+                    <div>
+                        <img className="carouselImg" src="https://cdnweb01.wikitree.co.kr/webdata/editor/201710/15/img_20171015122006_4a43f846.jpg"/>
+                    </div>
+                </Carousel>
+            </div>
+            <div className="imgList">
+                <List
+                    itemLayout="vertical"
+                    size="small"
+                    dataSource={listData}
+                    renderItem={item => (
+                        <List.Item
+                            key={item.title}
+                            extra={
+                                <img
+                                    width="300"
+                                    alt="logo"
+                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                                />
+                            }
+                        >
+                            <List.Item.Meta
+                                title={<a href={item.href}>{item.title}</a>}
+                            />
+                            <h4>{item.content}</h4>
+                        </List.Item>
+                    )}
+                />
+            </div>
+
+        </div>
+    )
 }
 
 export  default MemoryPage;
